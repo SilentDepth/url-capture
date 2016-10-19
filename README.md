355ec2999d77df7b61b44c47460f804b94d7e4d7
SilentDepth <silentdepth@outlook.com>
2016-10-19 18:03:00 +0800

    Accept urls directly from command line arguments

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Capture screenshots for URLs using [PhantomJS](http://phantomjs.org/).
 
 ## How to use
 
-1. [Download this repo](https://github.com/SilentDepth/url-capture/archive/master.zip) (a zip-file) and extract the content to somewhere.
+1. [Download this tool](https://github.com/SilentDepth/url-capture/archive/master.zip) (a zip-file) and extract the content to somewhere.
 2. [Download PhantomJS](http://phantomjs.org/download.html) and add it into `PATH`. If you don't know how to set `PATH`, just extract the executable (i.e. `phantomjs.exe`) under `bin` folder to the place from step 1.
 3. Look at the `config.js` and change anything you need.
-4. Launch terminal from the folder you extract this repo to. Run command: `phantomjs index.js path/to/url-data-file`.
+4. Launch terminal from the folder you extract this tool to. Run command:
+    - If you have lots of URL to capture: `phantomjs index.js path/to/url-data-file`
+    - If you just want to capture some: `phantomjs index.js url1 url2 ...`
 5. When it is done you will see the images in `screenshots` folder.
 
 ## URL Data File
--- src/core/data-reader.js
+++ src/core/data-reader.js
@@ -6,18 +6,32 @@ var fs = require('fs');
 var config = require('../../config');
 
 module.exports = function () {
-  var list = [];
-
   // Validate arguments
   if (args.length === 1) {
-    console.log('No arguments found. Usage: phantomjs index.js path/to/urls.txt (or any other name)');
+    console.log([
+      'No arguments found.',
+      'Usage: phantomjs index.js path/to/url-data-file',
+      '       phantomjs index.js url1 url2 ...'
+    ].join('\n'));
     phantom.exit();
   }
 
   // Read data
-  var lines = fs.read(args[1]).split('\n');
-  if (config['has_title_row']) lines.shift();
+  var lines;
+  // If the first argument is a file, read the content
+  if (fs.isFile(args[1])) {
+    lines = fs.read(args[1]).split('\n');
+    if (config['has_title_row']) lines.shift();
+  // Otherwise treat it as URL
+  // TODO: Custom data format
+  } else {
+    lines = Array.prototype.slice.call(args, 1).map(function (url, idx) {
+      return (idx + 1) + ' ' + url;
+    });
+  }
 
+  // Extract URLs
+  var list = [];
   var tokens;
   lines.forEach(function (line) {
     // TODO: Custom separator
